df9b76442761f0d89e6f9d4df3257dc266afdac4
Kristian Hellang <kristian@hellang.com>
2015-01-14 20:24:10 +0100

    Added icon

--- src/ResourceHelperGenerator/Properties/AssemblyInfo.cs
+++ src/ResourceHelperGenerator/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Contains an MSBuild task that generates a strongly typed helper class for resource files with support for string formatting.")]
 [assembly: AssemblyCompany("Kristian Hellang")]
 [assembly: AssemblyProduct("ResourceHelperGenerator")]
-[assembly: AssemblyCopyright("Copyright © Kristian Hellang 2014")]
+[assembly: AssemblyCopyright("Copyright © Kristian Hellang 2015")]
 
 [assembly: Guid("49db0a2b-ff6b-4c51-aa4a-88d23aaf68a7")]
 
